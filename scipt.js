c06de77f67099e4e74222c9d1d2ec9c58948c50b
mehzabin-haque <bsse1233@iit.du.ac.bd>
2023-02-28 11:02:19 +0600

    change

--- scipt.js
+++ scipt.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".typing", {
-    strings: ["Undergrad Student", "Web Developer", "Creative", "Enthusiastic"],
+    strings: ["", "Undergrad Student", "Web Developer", "Creative", "Enthusiastic"],
     typespeed: 100,
     Backspeed: 60,
     loop: true
