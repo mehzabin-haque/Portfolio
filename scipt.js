5cc7384d8c56e828f33daa9acb86716c39db4983
mehzabin-haque <bsse1233@iit.du.ac.bd>
2023-02-28 23:24:14 +0600

    tt

--- scipt.js
+++ scipt.js
@@ -1,8 +1,8 @@
 var typed = new Typed(".typing", {
     strings: ["an Undergrad student", " a Web Developer", " a Creative Person", "an Enthusiastic person"],
     typespeed: 100000000000,
-    Backspeed: 60000000000,
-    backDelay: 1000,
+    Backspeed: 75000000000,
+    backDelay: 1500,
     startDelay: 2000,
     loop: true
 });
